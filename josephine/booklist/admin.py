a1c5d1de23614b2f49b6d2e6a5353c4971d578eb
Judith van Stegeren <git@jd7h.com>
2019-11-28 18:50:36 +0100

    Add models for Books, Status and StatusUpdates

--- josephine/booklist/admin.py
+++ josephine/booklist/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Book, Status, StatusUpdate
+
+admin.site.register(Book)
+admin.site.register(Status)
+admin.site.register(StatusUpdate)
+
--- josephine/booklist/models.py
+++ josephine/booklist/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.author + " - " + self.title
+
+
+class Status(models.Model):
+    status_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.status_name
+
+class StatusUpdate(models.Model):
+    date = models.DateTimeField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    new_status = models.ForeignKey(Status, on_delete=models.CASCADE)
+
+    def __str__(self):
+        date_repr = self.date.strftime("%d %b %Y")
+        return date_repr + ": " + str(self.book) + " has new status " + str(self.new_status)
