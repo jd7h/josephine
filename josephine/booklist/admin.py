42f2bbd58f077811877296901cf548e2fba046fc
Judith van Stegeren <git@jd7h.com>
2019-12-02 14:55:03 +0100

    Add ReadDate and Rating classes
    Fix issue #6 and #7

--- josephine/booklist/admin.py
+++ josephine/booklist/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Book, Status, StatusUpdate
+from .models import Book, Status, StatusUpdate, ReadDate, Rating
 
 admin.site.register(Book)
 admin.site.register(Status)
 admin.site.register(StatusUpdate)
-
+admin.site.register(ReadDate)
+admin.site.register(Rating)
--- josephine/booklist/models.py
+++ josephine/booklist/models.py
@@ -26,3 +26,25 @@ class StatusUpdate(models.Model):
     def __str__(self):
         date_repr = self.date.strftime("%d %b %Y")
         return date_repr + ": " + str(self.book) + " has new status " + str(self.new_status)
+
+class ReadDate(models.Model):
+    date = models.DateField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        date_repr = self.date.strftime("%d %b %Y")
+        return date_repr + ": read " + str(self.book)
+
+class Rating(models.Model):
+    class StarRating(models.IntegerChoices):
+        ONE_STAR = 1
+        TWO_STARS = 2
+        THREE_STARS = 3
+        FOUR_STARS = 4
+        FIVE_STARS = 5
+
+    rating = models.IntegerField(choices=StarRating.choices)
+    book = models.OneToOneField(Book, on_delete=models.CASCADE, primary_key=True)
+   
+    def __str__(self):
+        return str(self.rating) + " stars for " + str(self.book)
