3cd0c7771343cd19917fb28c291deba1b0758e37
Judith van Stegeren <git@jd7h.com>
2019-12-03 22:13:16 +0100

    Stubs for all basic views, urls and index template
    Start with #17, #18, #19

--- josephine/booklist/urls.py
+++ josephine/booklist/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('all/', views.all, name='all'),
+    path('<int:book_id>/', views.detail, name='detail'),
+    path('shelf/<int:shelf_id>/', views.shelf, name='shelf'),
+    path('status/<int:status_id>/', views.status, name='status'),
 ]
--- josephine/booklist/views.py
+++ josephine/booklist/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Book
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. This is going to be the index of josephine.")
+    latest_books = Book.objects.order_by('id')[:5]
+    template = loader.get_template('booklist/index.html')
+    context = {
+        'latest_books' : latest_books,
+    }
+    output = "<br>".join([str(book) for book in latest_books])
+    return HttpResponse(template.render(context, request))
+
+def detail(request, book_id):
+    return HttpResponse("You're looking at the details of book %s." % book_id)
+
+def all(request):
+    return HttpResponse("Here you can view all books in the database")
+
+def edit(request, book_id):
+    return HttpResponse("You're editing book %s." % book_id)
+
+def status(request, status_id):
+    response = "You're looking at all books with status %s."
+    return HttpResponse(response % status_id)
+
+def shelf(request, shelf_id):
+    response = "You're looking at all books in category %s."
+    return HttpResponse(response % shelf_id)
+
+
