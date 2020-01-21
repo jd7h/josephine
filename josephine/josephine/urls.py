12b2c7f3903d1f0f3c1cac55d60f070e88fd13f0
Judith van Stegeren <git@jd7h.com>
2020-01-21 21:47:37 +0100

    Use / for booklist app

--- josephine/josephine/urls.py
+++ josephine/josephine/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('books/', include('booklist.urls')),
+    path('', include('booklist.urls')),
     path('admin/', admin.site.urls),
 ]
