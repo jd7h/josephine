c0f270c6b93fe763464bc97f59bc21b64226c365
Judith van Stegeren <git@jd7h.com>
2020-01-21 21:37:10 +0100

    add namespace to urls

--- josephine/booklist/urls.py
+++ josephine/booklist/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'booklist'
 urlpatterns = [
     path('', views.index, name='index'),
     path('all/', views.all, name='all'),
     path('<int:book_id>/', views.detail, name='detail'),
     path('shelf/<int:shelf_id>/', views.shelf, name='shelf'),
     path('status/<int:status_id>/', views.status, name='status'),
+    path('<int:book_id>/rate/', views.rate, name='rate'),
+
 ]
