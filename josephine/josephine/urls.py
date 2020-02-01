730398ab9ae2e152e55bd786cc3fdf769c6724fc
Judith van Stegeren <git@jd7h.com>
2020-02-02 00:12:25 +0100

    Add lightweight formatting to templates, setup static files

--- josephine/josephine/urls.py
+++ josephine/josephine/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('booklist.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # not for production, see https://overiq.com/django-1-10/handling-media-files-in-django/
